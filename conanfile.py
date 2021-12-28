784225e8d11c985fe433fc240f3b0580c409a9c2
Steve Robinson <ssrobins@gmail.com>
2021-12-27 17:47:22 -0800

    Go back to Qt5 for better old platform compatibility

--- conanfile.py
+++ conanfile.py
@@ -9,5 +9,5 @@ class Conan(ConanFile):
     def requirements(self):
         self.requires("boost/1.78.0")
         self.requires("gtest/1.11.0")
-        #self.requires("qt/5.15.2")
-        self.requires("qt/6.2.2")
+        self.requires("qt/5.15.2")
+        #self.requires("qt/6.2.2")
